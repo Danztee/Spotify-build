ba5f64e6909e579ae844788185dc5fa259d1e7fd
Danztee <olowoniyidaniel@gmail.com>
2022-12-30 10:02:12 +0100

    cleaned up the index

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import styles from "../styles/Home.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -16,6 +11,12 @@ export default function Home() {
       </Head>
 
       <h1>this is a spotify build</h1>
+      <main>
+        {/* sidebar */}
+        {/* center */}
+      </main>
+
+      <footer>{/* player */}</footer>
     </>
   );
 }
